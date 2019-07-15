357298228bc4b28f2083fc764b4b956f93162def
heyKim <jkk3366@gmail.com>
2019-07-15 15:52:08 +0900

    코드 interface 추가

--- api/interface/commonCode/commonCode.controller.js
+++ api/interface/commonCode/commonCode.controller.js
@@ -1,6 +1,18 @@
 const models = require('../../model/commonCode/CommonCode');
 const systemMessage = require('../../../config/systemMessage');
 
+/** 코드 리스트 조회 */
+exports.list = (req,res) => {
+  return models.CommonCode.findAll({
+    where : {
+
+    }, order: [['groupId', 'ASC'], ['codeOrder', 'ASC']]
+  }).then(codeList => {
+    return res.json(codeList)
+  }).catch( function(err) {
+    console.log(err);
+  });
+}
 exports.index = (req,res) => {
   const groupId = req.params.groupId || '';
   const codeOption = req.params.codeOption || '';
@@ -35,3 +47,29 @@ exports.index = (req,res) => {
       return res.status(500).json(err)
   });
 };
+
+/** 코드 업데이트 */
+exports.update = (req,res) => {
+  const codeName = req.params.codeName || '';
+  const codeValue = req.body.codeValue || '';
+
+  if(!codeName.length){
+    return res.status(400).json({error:systemMessage.search.incorrectKey + "codeName" , req:codeName});
+  }
+  if(!codeValue.length){
+    return res.status(400).json({error:systemMessage.search.incorrectKey + "codeValue" , req:codeValue});
+  }
+
+  return models.CommonCode.update({
+      codeValue: codeValue
+    },{
+        where : { codeName: codeName }
+    }).then( () => {
+        return res.status(200).json("수정이 완료되었습니다.");
+      }
+    ).catch((err) => {
+      console.log(err);
+      return res.status(500).json(err)
+    })
+
+}
--- api/interface/commonCode/index.js
+++ api/interface/commonCode/index.js
@@ -6,5 +6,7 @@ const controller = require('./commonCode.controller');
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
 
+router.get('/', controller.list);
 router.get('/:groupId/:codeOption', controller.index);
+router.put('/:codeName', controller.update)
 module.exports = router;
